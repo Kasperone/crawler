1f121fb5a8bca353dd27f6d78bfb71a52db78f60
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-07-30 22:39:59 +0200

    wip c3

--- crawler.py
+++ crawler.py
@@ -11,13 +11,17 @@ def extract_links(url):
     response = requests.get(url)
     return re.findall('(?:href=")(.*?)"', str(response.content))
 
-href_links = extract_links(target_url)
-for link in href_links:
-    link = urlparse.urljoin(target_url, link)
+def crawl(url):
+    href_links = extract_links(url)
+    for link in href_links:
+        link = urlparse.urljoin(url, link)
 
-    if "#" in link:
-        link = link.split("#")[0]
+        if "#" in link:
+            link = link.split("#")[0]
 
-    if target_url in link and link not in target_links:
-        target_links.append(link)
-        print(link)
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+crawl(target_url)
