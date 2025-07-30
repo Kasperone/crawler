81f2f821ae2cc2b7b8fcafe12747af991e36fed0
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-07-30 22:04:46 +0200

    wip c2

--- crawler.py
+++ crawler.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
 import requests
+import re
+import urllib.parse as urlparse
 
-def request(url):
-    try:
-        return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
-        pass
+target_url = "http://xxx.xxx.xxx.xxx/mutillidae/"
+target_links = []
 
-target_url = "google.com"
+def extract_links(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', str(response.content))
 
-with open("/root/Downloads/subdomains.list", "r") as wordlist_file:
-    for line in wordlist_file:
-        word = line.strip()
-        test_url = word + "." + target_url
-        response = request(test_url)
-        if response:
-            print("[+] Discovered subdomain --> " + test_url)
+href_links = extract_links(target_url)
+for link in href_links:
+    link = urlparse.urljoin(target_url, link)
+
+    if "#" in link:
+        link = link.split("#")[0]
+
+    if target_url in link and link not in target_links:
+        target_links.append(link)
+        print(link)
